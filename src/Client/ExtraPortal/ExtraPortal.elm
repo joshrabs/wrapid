9a74896a9d35d20a10d757da6069addb28bbb683
Steven MacCoun <theraccoun@gmail.com>
2017-02-10 12:17:38 -0800

    Added avatar pic to extra portal

--- src/Client/ExtraPortal/ExtraPortal.elm
+++ src/Client/ExtraPortal/ExtraPortal.elm
@@ -20,17 +20,18 @@ type ViewState = ProfileWizard | FormStatus
 -- UPDATE
 type Msg = ChangeView ViewState
 
-
+defaultUrl: Maybe String
+defaultUrl = Just "https://files.graph.cool/ciykpioqm1wl00120k2e8s4la/ciyvfw6ab423z01890up60nza"
 --VIEW
 viewExtraPortal: Model -> Html Msg
 viewExtraPortal model =
   div []
       [
         let
-            rightItems = {avatar = Nothing}
+            rightItems = {avatar = Just defaultUrl}
         in
           Dashboard.view {navbar = {rightItems = Just rightItems}}
-      , h1 [] [text "Welcome to the extra portal!"]
+      , viewHeader {firstName="Steve", production="AMC's the Walking Dead"}
       ,
         let
           panelHeader = Just {title ="Schedule"}
@@ -41,7 +42,32 @@ viewExtraPortal model =
       ]
 
 
+type alias Header = {firstName: String, production: String}
+viewHeader: Header -> Html Msg
+viewHeader header =
+  div [style [("display", "flex"), ("flex-direction", "column"),("margin", "8px 4px 16px 16px")]]
+  [
+     span [] [text "Monday May 25th, 2017"]
+    , span [style headerTitleStyle] [text ("Welcome " ++ header.firstName)]
+    ,span [] [text (header.production)]
+  ]
 
+headerTitleStyle =
+  [
+  ("font-family", "Roboto-Bold")
+  ,("font-size", "32px")
+  ,("color", "#282C35")
+  ,("letter-spacing", "0")
+  ]
+
+headerProductionStyle =
+  [
+  ("font-family", "Roboto-Regular")
+  ,("font-size", "16px")
+  ,("color", "#282C35")
+  ,("letter-spacing", "0")
+  ,("line-height", "20px")
+  ]
 type alias Schedule = List String
 viewSchedule: Schedule -> Html Msg
 viewSchedule schedule =
