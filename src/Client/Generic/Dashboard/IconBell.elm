25dc3a1771fb278fa6e67d8c5684645529c25fcd
Steven MacCoun <theraccoun@gmail.com>
2017-02-15 14:26:42 -0800

    Dummy notification bell items for testing

--- src/Client/Generic/Dashboard/IconBell.elm
+++ src/Client/Generic/Dashboard/IconBell.elm
@@ -27,11 +27,17 @@ iconBell notifications =
           ]
         ][Html.text (notifications |> List.length |> toString)]
       ]
-  , svg [ Svg.Attributes.height "18px", viewBox "6 10 12 15", Svg.Attributes.width "16px" ]
+  , svg [Svg.Attributes.height "18px", viewBox "6 10 12 15", Svg.Attributes.width "16px" ]
       [Svg.path [ d "M12,24.625 C12.825,24.625 13.5,23.95 13.5,23.125 L10.5,23.125 C10.5,23.95 11.1675,24.625 12,24.625 Z M16.5,20.125 L16.5,16.375 C16.5,14.0725 15.27,12.145 13.125,11.635 L13.125,11.125 C13.125,10.5025 12.6225,10 12,10 C11.3775,10 10.875,10.5025 10.875,11.125 L10.875,11.635 C8.7225,12.145 7.5,14.065 7.5,16.375 L7.5,20.125 L6,21.625 L6,22.375 L18,22.375 L18,21.625 L16.5,20.125 Z"
         , fill "#000000"
         , id "Shape"
         , stroke "none" ]
           []
       ]
+      ,viewNotificationList notifications
   ]
+
+
+viewNotificationList: IconNotifications -> Html msg
+viewNotificationList notifications =
+  div [] (List.map (\notification -> div [] [text notification.message]) notifications)
