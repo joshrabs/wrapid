2dc76f3a9d3aeadd63a6735955d54d2a5f0964bf
NicholasDunham <nmdnhm@gmail.com>
2017-02-15 15:40:23 -0800

    Create State.elm and View.elm

---
+++ src/State.elm
@@ -0,0 +1,57 @@
+module State exposing (..)
+
+import Navigation as Nav
+import Types exposing (..)
+import Client.ExtraPortal.ExtraPortal as ExtraPortal
+import Client.PAPortal.PAPortal as PAPortal
+
+
+init : Nav.Location -> ( Model, Cmd Msg )
+init location =
+    ( Model [ location ] Nothing LoginView ExtraPortal.initModel (PAPortal.initModel "word") "Yo"
+    , Cmd.none
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ChangeView viewState ->
+            case viewState of
+                LoginView ->
+                    ( { model | currentViewState = viewState }, Cmd.none )
+
+                ExtraPortalView ->
+                    ( { model | extraPortalModel = ExtraPortal.initModel, currentViewState = viewState }, Cmd.none )
+
+                PAPortalView ->
+                    ( { model | currentViewState = viewState }, Cmd.none )
+
+        UrlChange location ->
+            ( { model | history = location :: model.history }
+            , Cmd.none
+            )
+
+        LoginMsg loginMsg ->
+            ( model, Cmd.none )
+
+        ExtraPortalMsg epMsg ->
+            let
+                ( extraPortalModel, epCmd ) =
+                    ExtraPortal.update epMsg model.extraPortalModel
+            in
+                ( { model | extraPortalModel = extraPortalModel }, Cmd.map (\b -> ExtraPortalMsg b) epCmd )
+
+        PAPortalMsg paMsg ->
+            let
+                ( paPortalModel, paCmd ) =
+                    PAPortal.update paMsg model.paPortalModel
+            in
+                ( { model | paPortalModel = paPortalModel }, Cmd.map (\b -> PAPortalMsg b) paCmd )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Sub.map (\pas -> PAPortalMsg pas) (PAPortal.subscriptions model.paPortalModel)
+        ]
---
+++ src/View.elm
@@ -0,0 +1,41 @@
+module View exposing (view)
+
+import Types exposing (..)
+import Client.Generic.Authentication.Login.Login as Login
+import Client.ExtraPortal.ExtraPortal as ExtraPortal
+import Client.PAPortal.PAPortal as PAPortal
+import Html exposing (Html, a, button, div, h1, h4, img, li, p, text, ul)
+import Html.Attributes exposing (href, src, style)
+import Html.Events exposing (onClick)
+import Navigation as Nav
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ case model.currentViewState of
+            LoginView ->
+                Html.map LoginMsg (Login.loginView (Login.initModel Nothing Nothing))
+
+            ExtraPortalView ->
+                Html.map ExtraPortalMsg (ExtraPortal.viewExtraPortal model.extraPortalModel)
+
+            PAPortalView ->
+                Html.map PAPortalMsg
+                    (PAPortal.viewPAPortal model.paPortalModel)
+        , div [ style [ ( "position", "fixed" ), ( "top", "0px" ), ( "right", "0px" ), ( "border", "1px solid black" ) ] ]
+            [ button [ onClick (ChangeView ExtraPortalView) ] [ text "Extra Portal" ]
+            , button [ onClick (ChangeView PAPortalView) ] [ text "PA Portal" ]
+            , button [ onClick (ChangeView LoginView) ] [ text "Login" ]
+            ]
+        ]
+
+
+viewLink : String -> Html msg
+viewLink name =
+    li [] [ a [ href ("#" ++ name) ] [ text name ] ]
+
+
+viewLocation : Nav.Location -> Html msg
+viewLocation location =
+    li [] [ text (location.pathname ++ location.hash) ]
