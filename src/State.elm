ace770f01072749528c6fa65d491f489e876728d
Steven MacCoun <theraccoun@gmail.com>
2017-02-21 13:14:13 -0800

    Set current date in master with lazy update every hour

--- src/State.elm
+++ src/State.elm
@@ -5,6 +5,11 @@ import Navigation as Nav
 import Types exposing (..)
 import Client.ExtraPortal.ExtraPortal as ExtraPortal
 
+import Time exposing (Time, hour)
+import Date exposing (Date, fromTime)
+import Date.Extra.Compare as CompareDate exposing (Compare2(..))
+import Task exposing (perform)
+
 
 -- import Client.PAPortal.Types as PATypes
 
@@ -20,70 +25,91 @@ init : Nav.Location -> ( Model, Cmd Msg )
 init location =
     ( { history = [ location ]
       , currentImg = Nothing
+      , currentDate = Nothing
       , currentViewState = LoginView
       , extraPortalModel = (ExtraPortal.initModel defaultUserID)
       , paPortalModel = (PAState.initModel "word")
       , title = "Yo"
       , mdl = Material.model
       }
-    , Cmd.none
+    , now
     )
+
+now : Cmd Msg
+now =
+  Task.perform SetDate Date.now
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ChangeView viewState ->
-            case viewState of
-                LoginView ->
-                    ( { model | currentViewState = viewState }, Cmd.none )
+      SetDate newDate ->
+        ({model | currentDate = Just newDate}, Cmd.none)
+      Tick newTime ->
+        let
+          newDate = Date.fromTime newTime
+        in
+          case model.currentDate of
+            Just currentDate ->
+              if CompareDate.is Same currentDate newDate
+              then
+                (model, Cmd.none)
+              else
+                (model, now)
+            Nothing ->
+              (model, now)
+      ChangeView viewState ->
+          case viewState of
+              LoginView ->
+                  ( { model | currentViewState = viewState }, Cmd.none )
 
-                ExtraPortalView ->
-                    let
-                        ( extraPortalModel, cmd ) =
-                            ExtraPortal.update ExtraPortal.LoadRemoteData model.extraPortalModel
-                    in
-                        ( { model | extraPortalModel = extraPortalModel, currentViewState = viewState }, cmd )
+              ExtraPortalView ->
+                  let
+                      ( extraPortalModel, cmd ) =
+                          ExtraPortal.update ExtraPortal.LoadRemoteData model.extraPortalModel
+                  in
+                      ( { model | extraPortalModel = extraPortalModel, currentViewState = viewState }, cmd )
 
-                PAPortalView ->
-                    ( { model | currentViewState = viewState }, Cmd.none )
+              PAPortalView ->
+                  ( { model | currentViewState = viewState }, Cmd.none )
 
-        UrlChange location ->
-            ( { model | history = location :: model.history }
-            , Cmd.none
-            )
+      UrlChange location ->
+          ( { model | history = location :: model.history }
+          , Cmd.none
+          )
 
-        LoginMsg loginMsg ->
-            ( model, Cmd.none )
+      LoginMsg loginMsg ->
+          ( model, Cmd.none )
 
-        ExtraPortalMsg epMsg ->
-            let
-                ( extraPortalModel, epCmd ) =
-                    ExtraPortal.update epMsg model.extraPortalModel
-            in
-                ( { model | extraPortalModel = extraPortalModel }, Cmd.map (\b -> ExtraPortalMsg b) epCmd )
+      ExtraPortalMsg epMsg ->
+          let
+              ( extraPortalModel, epCmd ) =
+                  ExtraPortal.update epMsg model.extraPortalModel
+          in
+              ( { model | extraPortalModel = extraPortalModel }, Cmd.map (\b -> ExtraPortalMsg b) epCmd )
 
-        PAPortalMsg paMsg ->
-            let
-                ( paPortalModel, paCmd ) =
-                    PAState.update paMsg model.paPortalModel
-            in
-                ( { model | paPortalModel = paPortalModel }, Cmd.map (\b -> PAPortalMsg b) paCmd )
+      PAPortalMsg paMsg ->
+          let
+              ( paPortalModel, paCmd ) =
+                  PAState.update paMsg model.paPortalModel
+          in
+              ( { model | paPortalModel = paPortalModel }, Cmd.map (\b -> PAPortalMsg b) paCmd )
 
-        ToggleNotifications ->
-            ( model, Cmd.none )
+      ToggleNotifications ->
+          ( model, Cmd.none )
 
-        SelectNotification _ ->
-            ( model, Cmd.none )
+      SelectNotification _ ->
+          ( model, Cmd.none )
 
-        Mdl message_ ->
-            Material.update Mdl message_ model
+      Mdl message_ ->
+          Material.update Mdl message_ model
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ Sub.map PAPortalMsg (PAState.subscriptions model.paPortalModel)
+        [ Time.every hour Tick
+        , Sub.map PAPortalMsg (PAState.subscriptions model.paPortalModel)
         , Sub.map ExtraPortalMsg (ExtraPortal.subscriptions model.extraPortalModel)
         , Material.subscriptions Mdl model
         ]
--- src/Types.elm
+++ src/Types.elm
@@ -5,10 +5,13 @@ import Navigation as Nav
 import Client.ExtraPortal.ExtraPortal as ExtraPortal
 import Client.PAPortal.Types as PAPortal
 import Client.Generic.Authentication.Login.Types as Login
+import Time exposing (Time)
+import Date exposing (Date, fromTime)
 
 
 type alias Model =
     { history : List Nav.Location
+    , currentDate: Maybe Date
     , currentImg : Maybe String
     , currentViewState : ViewState
     , extraPortalModel : ExtraPortal.Model
@@ -20,6 +23,8 @@ type alias Model =
 
 type Msg
     = UrlChange Nav.Location
+    | Tick Time
+    | SetDate Date
     | LoginMsg Login.Msg
     | ChangeView ViewState
     | ExtraPortalMsg ExtraPortal.Msg
