943c530b57fc4753339e5097dd415f1d59977d75
mediocreHacker <mediocreHacker@protonmail.com>
2017-03-04 19:20:59 +0100

    elm-format

--- src/Types.elm
+++ src/Types.elm
@@ -13,20 +13,28 @@ import Http exposing (..)
 
 type alias Model =
     { history : List Nav.Location
-    , currentDate: Maybe Date
+    , currentDate : Maybe Date
     , currentImg : Maybe String
     , currentViewState : ViewModelState
     , title : String
     , mdl : Material.Model
-    , shouldShowPortalSwitcher: Bool --Note this is only for development!
+    , shouldShowPortalSwitcher :
+        Bool
+        --Note this is only for development!
     }
+
 
 type ViewModelState
     = Login Login.Model
     | ExtraPortal ExtraPortal.Model
     | PAPortal PAPortal.Model
 
-type ViewMsg = LoginView | PAPortalView | ExtraPortalView
+
+type ViewMsg
+    = LoginView
+    | PAPortalView
+    | ExtraPortalView
+
 
 type Msg
     = UrlChange Nav.Location
@@ -39,14 +47,13 @@ type Msg
     | SelectNotification String
     | Mdl (Material.Msg Msg)
     | ShowPortalSwitcher Bool
-    --| ReceiveAuthentication (Result Http.Error String)
+      --| ReceiveAuthentication (Result Http.Error String)
     | ReceiveAuthentication (RemoteData.WebData String)
+
 
 type ChildPortalMsg
     = ExtraPortalMsg ExtraPortal.Msg
     | PAPortalMsg PAPortal.Msg
-
-
 
 
 type alias Url =
