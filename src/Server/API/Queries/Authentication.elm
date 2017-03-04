de69432dc1a51a1d5f90b234a29f58fd4811976d
mediocreHacker <mediocreHacker@protonmail.com>
2017-03-04 19:07:59 +0100

    elm-format

--- src/Server/API/Queries/Authentication.elm
+++ src/Server/API/Queries/Authentication.elm
@@ -5,8 +5,15 @@ import Json.Encode as Encode exposing (encode, object, string)
 import Json.Decode as Decode exposing (list, string)
 import RemoteData exposing (sendRequest)
 
-type alias Username = String
-type alias Password = String
+
+type alias Username =
+    String
+
+
+type alias Password =
+    String
+
+
 
 -- loginPostReq : Body -> Request String
 -- loginPostReq body =
@@ -24,24 +31,31 @@ type alias Password = String
 --     , withCredentials = False
 --     }
 
-loginBody: Username -> Password -> Http.Body
+
+loginBody : Username -> Password -> Http.Body
 loginBody username password =
-  jsonBody
-    (object
-      [("auth", Encode.object
-        [ ("email", Encode.string username)
-        , ("password", Encode.string password)
-        ]
+    jsonBody
+        (object
+            [ ( "auth"
+              , Encode.object
+                    [ ( "email", Encode.string username )
+                    , ( "password", Encode.string password )
+                    ]
+              )
+            ]
         )
-      ]
-    )
+
+
 
 -- loginUser: (Result Error String -> msg) -> Username -> Password -> Cmd msg
 -- loginUser cmsg username password =
 --   Http.send cmsg (loginPostReq (loginBody username password))
 
-loginUser: Username -> Password ->  Cmd (RemoteData.WebData String)
+
+loginUser : Username -> Password -> Cmd (RemoteData.WebData String)
 loginUser username password =
     Http.post
-      "https://35.157.165.22//user_token" (loginBody username password) Decode.string
+        "https://35.157.165.22//user_token"
+        (loginBody username password)
+        Decode.string
         |> RemoteData.sendRequest
