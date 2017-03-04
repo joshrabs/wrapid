677129369800e03bf3ed2d59980ed101163be5c6
mediocreHacker <mediocreHacker@protonmail.com>
2017-03-04 20:43:02 +0100

    elm-format

--- src/Client/Generic/Authentication/Login/View.elm
+++ src/Client/Generic/Authentication/Login/View.elm
@@ -15,37 +15,39 @@ loginView : Model -> Html Msg
 loginView model =
     div []
         [ Dashboard.view { navbar = { rightItems = Nothing } }
-        , div [style [
-            ("display", "flex")
-            ,("flex-direction", "column")
-            , ("justify-content", "center")
-            , ("align-items", "center")
-            , ("margin", "16px")
-          ]]
-          [span [style headerTitleStyle] [text "Welcome"]
-          ,span
-              [ style
-                  [ ( "font-family", "Roboto-Regular" )
-                  , ( "font-size", "16px" )
-                  , ( "margin", "8px 0px 8px 0px" )
-                  , ( "color", "#282C35" )
-                  , ( "letter-spacing", "0" )
-                  ]
-              ]
-              [ text "RUNABETTERSET Productions" ]
-          ]
-        ,viewLoginPanel model
+        , div
+            [ style
+                [ ( "display", "flex" )
+                , ( "flex-direction", "column" )
+                , ( "justify-content", "center" )
+                , ( "align-items", "center" )
+                , ( "margin", "16px" )
+                ]
+            ]
+            [ span [ style headerTitleStyle ] [ text "Welcome" ]
+            , span
+                [ style
+                    [ ( "font-family", "Roboto-Regular" )
+                    , ( "font-size", "16px" )
+                    , ( "margin", "8px 0px 8px 0px" )
+                    , ( "color", "#282C35" )
+                    , ( "letter-spacing", "0" )
+                    ]
+                ]
+                [ text "RUNABETTERSET Productions" ]
+            ]
+        , viewLoginPanel model
         ]
+
 
 headerTitleStyle : List ( String, String )
 headerTitleStyle =
-  [
-  ("font-family", "Roboto-Bold")
-  ,("font-size", "32px")
-  ,("color", "#282C35")
-  ,("letter-spacing", "0")
-  ,("margin", "4px 0px 8px 0px")
-  ]
+    [ ( "font-family", "Roboto-Bold" )
+    , ( "font-size", "32px" )
+    , ( "color", "#282C35" )
+    , ( "letter-spacing", "0" )
+    , ( "margin", "4px 0px 8px 0px" )
+    ]
 
 
 viewLoginPanel : Model -> Html Msg
@@ -55,7 +57,7 @@ viewLoginPanel model =
             Just { title = "Login", rightItem = Nothing }
 
         panelBody =
-            div [ style [ ( "display", "flex" ), ( "flex-direction", "column" ), ("margin", "8px") ] ]
+            div [ style [ ( "display", "flex" ), ( "flex-direction", "column" ), ( "margin", "8px" ) ] ]
                 [ Textfield.render Mdl
                     [ 10, 0 ]
                     model.mdl
@@ -78,41 +80,41 @@ viewLoginPanel model =
                 |> Material.Scheme.top
 
         footer =
-            Just (div [style [("display", "flex"), ("flex-direction", "row-reverse")]]
-            [
-              div [style loginButtonStyle]
-              [case model.viewState of
-                Submitting ->
-                  div [] [text "Logging you in..."]
-                InUse ->
-                  span [onClick SubmitLogin, style loginButtonTextStyle] [text "Sign In"]
-              ]
-            ])
-    in
-        div [style [("margin", "8px"), ("width", "360px")]] [Dashboard.makePanel panelHeader panelBody footer]
+            Just
+                (div [ style [ ( "display", "flex" ), ( "flex-direction", "row-reverse" ) ] ]
+                    [ div [ style loginButtonStyle ]
+                        [ case model.viewState of
+                            Submitting ->
+                                div [] [ text "Logging you in..." ]
 
+                            InUse ->
+                                span [ onClick SubmitLogin, style loginButtonTextStyle ] [ text "Sign In" ]
+                        ]
+                    ]
+                )
+    in
+        div [ style [ ( "margin", "8px" ), ( "width", "360px" ) ] ] [ Dashboard.makePanel panelHeader panelBody footer ]
 
 
 loginButtonStyle : List ( String, String )
 loginButtonStyle =
-  [
-     ("display", "flex")
-    ,("justify-content", "center")
-    ,("align-items", "center")
-    ,("background", "#50E3C2")
-    ,("box-shadow", "0 2px 2px 0 #C3C6CF")
-    ,("border-radius", "2px")
-    ,("border-color", "transparent")
-    ,("height", "48px")
-    ,("width", "114px")
-    ,("margin", "8px")
-  ]
+    [ ( "display", "flex" )
+    , ( "justify-content", "center" )
+    , ( "align-items", "center" )
+    , ( "background", "#50E3C2" )
+    , ( "box-shadow", "0 2px 2px 0 #C3C6CF" )
+    , ( "border-radius", "2px" )
+    , ( "border-color", "transparent" )
+    , ( "height", "48px" )
+    , ( "width", "114px" )
+    , ( "margin", "8px" )
+    ]
+
 
 loginButtonTextStyle : List ( String, String )
 loginButtonTextStyle =
-  [
-     ("font-family", "Roboto-Medium")
-    ,("font-size", "16px")
-    ,("color", "#FFFFFF")
-    ,("margin", "12px 8px 12px 8px")
-  ]
+    [ ( "font-family", "Roboto-Medium" )
+    , ( "font-size", "16px" )
+    , ( "color", "#FFFFFF" )
+    , ( "margin", "12px 8px 12px 8px" )
+    ]
