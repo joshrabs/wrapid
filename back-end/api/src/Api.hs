db7fb115eb851c4834625c6cfe82b2100c0edd4e
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-03-22 00:01:52 +0200

    add swagger generator for hs side

--- back-end/api/src/Api.hs
+++ back-end/api/src/Api.hs
@@ -18,6 +18,7 @@ module Api ( restAPIv1
 import qualified Aws
 import qualified Aws.Core                              as Aws
 import qualified Aws.S3                                as S3
+import qualified Control.Lens                          as L
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Resource          (runResourceT)
@@ -28,29 +29,54 @@ import qualified Data.ByteString.Lazy                  as BSL
 import           Data.Maybe
 import           Data.Monoid
 import           Data.Proxy
+import           Data.Swagger
+import           Data.Swagger.Internal
+import           Data.Swagger.Schema
+import           Data.Swagger.SchemaOptions
 import           Data.Text                             as T
 import           Database.PostgreSQL.Simple
 import           GHC.Generics
 import           Network.HTTP.Client.MultipartFormData
-import           Network.HTTP.Conduit                  (RequestBody (..),
-                                                        newManager,
-                                                        tlsManagerSettings,
-                                                        withManager)
+import qualified Network.HTTP.Conduit                  as HC
 import           Servant
 import           Servant.Multipart
+import           Servant.Server
+import           Servant.Swagger
 import           System.Directory
 import           System.IO
 import           System.Posix.Files
 
-import qualified Db                                    as Db
-import           Common.Types.User
 import           Common.Types.Extra
+import           Common.Types.Schedule
+import           Common.Types.User
+import qualified Db                                    as Db
 
 -----------------------------------------------------------------------------
 
+instance ToSchema Extra
+instance ToSchema Schedule
+instance ToSchema Event
+
+-- "/1/user"                               - get  - getUser (by email)
+-- "/1/set/:uuid/extra"                    - post - createExtra (Extra payload)
+-- "/1/set/:uuid/extra"                    - get  - getExtra
+-- "/1/set/:uuid/schedule"                 - post - createSchedule (Schedule payload)
+-- "/1/set/:uuid/schedule"                 - get  - getSchedule
+-- "/1/set/:uuid/shedule/event"            - post - createEvent (Event payload)
+-- "/1/set/:uuid/shedule/event/:id"        - get  - getEvent
+-- "/1/set/:uuid/shedule/event/:id/delete" - get  - deleteEvent
+
 type APIv1 =
   "1":>
-    (
+    (    CommonAPIv1
+    :<|> SwaggerAPI
+    )
+
+type SwaggerAPI =
+      "api"
+      :> Get '[JSON] Swagger
+
+type CommonAPIv1 =
          "user"
       :> Capture "email" Text
       :> Get '[JSON] User
@@ -65,26 +91,36 @@ type APIv1 =
       :> Capture "uuid" Text  -- ^ production set id
       :> "extra"
       :> Get '[JSON] Extra
-    
-    :<|> "set" 
+
+    :<|> "set"
      :> Capture "uuid" Text   -- ^ production set id
      :> "shedule"
      :> ReqBody '[JSON] Schedule
      :> Post    '[JSON] Schedule
 
-    :<|> "set" 
+    :<|> "set"
      :> Capture "uuid" Text   -- ^ production set id
      :> "shedule"             -- ^ returns only schedule for today
      :> Get '[JSON] Schedule
-    
+
     :<|> "set"
       :> Capture "uuid" Text
-      :> "schedule"           -- ^ adds event only for today's schedule
-      :> "event"
+      :> "schedule" :> "event"  -- ^ adds event only for today's schedule
       :> ReqBody '[JSON] Event
       :> Post    '[JSON] Event
-      
-    )
+
+    :<|> "set"
+      :> Capture "uuid" Text
+      :> "schedule" :> "event"
+      :> Capture "id" Text
+      :> Get '[JSON] Event
+
+    :<|> "set"
+      :> Capture "uuid" Text
+      :> "schedule" :> "event"
+      :> Capture "id" Text
+      :> "delete"
+      :> Get '[JSON] Bool
 
 restAPIv1 :: Proxy APIv1
 restAPIv1 = Proxy
@@ -94,8 +130,11 @@ server cc =  getUser  cc
         :<|> addExtra cc
         :<|> getExtra cc
         :<|> addSchedule cc
-        :<|> getShedule cc
+        :<|> getSchedule cc
         :<|> addEvent cc
+        :<|> getEvent cc
+        :<|> deleteEvent cc
+        :<|> generateSwagger
 
 --------------------------------------------------------------------------------
 -- Handlers
@@ -106,8 +145,8 @@ getUser cc email = undefined
 addExtra :: Db.ConnectConfig -> Text -> Extra -> Handler Extra
 addExtra cc uuid extra = undefined
 
-addExtra :: Db.ConnectConfig -> Text -> Handler Extra
-addExtra cc uuid = undefined
+getExtra :: Db.ConnectConfig -> Text -> Handler Extra
+getExtra cc uuid = undefined
 
 addSchedule :: Db.ConnectConfig -> Text -> Schedule -> Handler Schedule
 addSchedule cc uuid sched = undefined
@@ -118,3 +157,17 @@ getSchedule cc uuid = undefined
 addEvent :: Db.ConnectConfig -> Text -> Event -> Handler Event
 addEvent cc uuid event = undefined
 
+getEvent :: Db.ConnectConfig -> Text -> Text -> Handler Event
+getEvent cc uuid eid = undefined
+
+deleteEvent :: Db.ConnectConfig -> Text -> Text -> Handler Bool
+deleteEvent cc uuid eid = undefined
+
+generateSwagger =
+  return $
+    toSwagger (Proxy :: Proxy CommonAPIv1)
+      L.& info.title        L..~ "Common API"
+      L.& info.version      L..~ "1.0"
+      L.& info.description  L.?~ "This is a Common API service desription"
+      L.& info.license      L.?~ "AllRightsReserved"
+      L.& host              L.?~ "runabetterset.com"
