0f427ced9e5f5d74b42ab9fa66080e46eb6f73d5
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-03-22 21:24:52 +0200

    update records conversion

--- back-end/common/src/Common/Types/Extra.hs
+++ back-end/common/src/Common/Types/Extra.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards     #-}
 
 module Common.Types.Extra where
 
 import           Control.Applicative
 import           Data.Aeson
 import qualified Data.Default                         as Def
+import           Data.Csv                             as Csv
 import           Data.Maybe                           (fromJust, listToMaybe)
+import qualified Data.ByteString.Lazy                 as BSL
 import           Data.Text
 import           Data.Time.Clock
 import           Database.PostgreSQL.Simple.FromField
@@ -27,7 +30,7 @@ data Extra =
         , etype       :: Text          -- ^ TODO: should be extra datatype
         , role        :: Text          -- ^ TODO: should be role datatype
         , call        :: Text           
-        , rate        :: Text          -- ^ rate per hour
+        , rate        :: Int           -- ^ rate per hour
         , name        :: Text          -- ^ 
         , contact     :: Text          -- ^ email, phone, etc.
         , notes       :: Text          -- ^ additiona notes
@@ -38,3 +41,26 @@ instance ToJSON   Extra
 
 instance FromRow Extra
 instance ToRow   Extra
+
+instance FromNamedRecord Extra where
+  parseNamedRecord m =
+    Extra
+      <$> m Csv..: "Rate"
+      <*> m Csv..: "Type"
+      <*> m Csv..: "Role"
+      <*> m Csv..: "Call"
+      <*> m Csv..: "Rate"
+      <*> m Csv..: "Name"
+      <*> m Csv..: "Contact"
+      <*> m Csv..: "Notes"
+
+instance ToNamedRecord Extra where
+  toNamedRecord (Extra {..}) = Csv.namedRecord
+    [ "Type"      Csv..= etype 
+    , "Role"      Csv..= role
+    , "Name"      Csv..= name
+    , "Call"      Csv..= call
+    , "Rate"      Csv..= rate  
+    , "Contacts"  Csv..= contact  
+    , "Notes"     Csv..= notes
+    ]
