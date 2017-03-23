750a802e75fc8e30ecdd5d0a6db1ca2f0e910e38
Steven MacCoun <theraccoun@gmail.com>
2017-03-23 14:42:52 -0700

    Created live extra activity function and supporting tables

--- db/queries/get_daily_skin.sql
+++ db/queries/get_daily_skin.sql
@@ -1,28 +1,15 @@
 /*
-SAMPLE USAGE
+SAMPLE USAGE:
 --------------
-SELECT
-  effective_dt,
-  email,
-  full_name,
-  call_start_ts,
-  role,
-  rate
-FROM get_daily_skin('2017-03-21', 'RunaBetterSet');
+SELECT * FROM get_daily_skin('2017-03-21', 'RunaBetterSet');
 
-DROP FUNCTION get_daily_skin(DATE);
+MAINTENANCE:
+------------
+DROP FUNCTION get_daily_skin(DATE, VARCHAR);
+
  */
 
-
-
-CREATE FUNCTION get_daily_skin (p_effective_dt date, p_production_set_id VARCHAR(100))
-  RETURNS TABLE(
-    effective_dt date
-  , email character varying
-  , full_name character varying
-  , call_start_ts timestamp without time zone
-  , role character varying
-  , rate character varying)
+CREATE OR REPLACE FUNCTION get_daily_skin (p_effective_dt date, p_production_set_id character varying) RETURNS TABLE(effective_dt date, email character varying, full_name character varying, call_start_ts timestamp without time zone, role character varying, rate character varying)
 	LANGUAGE sql
 AS $$
     SELECT
