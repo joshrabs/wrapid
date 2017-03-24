ec740478e7287aa9b1b2ddfda83fda930522e211
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-03-24 18:27:47 +0200

    add tests for remote Api

---
+++ back-end/api-upload/test/main.sh
@@ -0,0 +1,14 @@
+echo "-------------------------------------------------------"
+echo "Upload image"
+uss=$(curl -i --form file1=@assets/test.png --form press=submit https://api.runabetterset.com/1/upload)
+echo "$uss"
+
+echo "-------------------------------------------------------"
+echo "Upload skin"
+sss=$(curl -i --form file1=@assets/skin-test.csv --form press=submit https://api.runabetterset.com/1/upload/set/runabetterset/skin)
+echo "$sss"
+
+echo "-------------------------------------------------------"
+echo "Get skin"
+pss=$(curl -i -X GET https://api.runabetterset.com/1/set/runabetterset/skin )
+echo "$pss"
